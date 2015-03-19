d5cc72a02b289d1877aff9f67ae7a4b558372e72
andy02081224 <andy02081224@gmail.com>
2015-03-19 22:17:44 +0800

    Change css rules to adapt windowUI to different size desktops

--- public/js/main.js
+++ public/js/main.js
@@ -22,4 +22,11 @@
 		$shortcutButton.toggleClass('active');
 	});
 
+	$exitButton.click(function() {
+		var exitForSure = confirm('安裝程式正在進行，確定關閉視窗？');
+		if (exitForSure) {
+
+		}
+
+	});
 })(jQuery, window)
