2fb085fc6dda48e76a0dded563b4868f54313aee
andy02081224 <andy02081224@gmail.com>
2015-03-22 16:44:59 +0800

    Add package-list page

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,14 +2,45 @@ var express = require('express'),
   router = express.Router(),
   Article = require('../models/article');
 
+  var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..');
+    var hbs;
+
+
 module.exports = function (app) {
-  app.use('/', router);
+
+ 
+
+	hbs = app.get('hbs');
+  console.log(rootPath + '/views/layouts/main.handlebars');
+
+  hbs.render(rootPath + '/views/index.handlebars', {title: '編譯過的'}).then(function(p) {
+  	console.log(p);
+  });
+
+	hbs.getTemplate(rootPath + '/views/index.handlebars').then(function (pages) {
+	    console.log('hi' + pages());
+	});
+
+		app.use('/', router);
+ 
 };
 
+
+
 router.get('/', function (req, res, next) {
-  var articles = [new Article(), new Article()];
+  // var articles = [new Article(), new Article()];
+  
+   //  hbs.render(rootPath + '/views/index.handlebars', {title: 'NBA'}).then(function(p) {
+   //  	res.content = p;
+  	// 	next();
+  	// });
+
     res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
+      title: 'Compiled template'
     });
 });
+
+// router.get('/', function (req, res) {
+// 	res.send(res.content);
+// });
